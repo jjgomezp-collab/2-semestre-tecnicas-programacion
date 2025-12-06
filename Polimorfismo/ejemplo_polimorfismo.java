0d0cd144ec60371beb63afc483ac9a650a543968
Jostyn Javier Gomez Paredes <jj.gomezp@uea.edu.ec>
2025-12-06 15:24:17 -0600

    Update ejemplo_polimorfismo.java

--- Polimorfismo/ejemplo_polimorfismo.java
+++ Polimorfismo/ejemplo_polimorfismo.java
@@ -1,1 +1,30 @@
+class Animal {
+    public void hacerSonido() {
+        System.out.println("Sonido genérico");
+    }
+}
+
+class Perro extends Animal {
+    @Override
+    public void hacerSonido() {
+        System.out.println("Guau guau");
+    }
+}
+
+class Gato extends Animal {
+    @Override
+    public void hacerSonido() {
+        System.out.println("Miau miau");
+    }
+}
+
+public class ejemplo_polimorfismo {
+    public static void main(String[] args) {
+        Animal animal1 = new Perro();
+        Animal animal2 = new Gato();
+
+        animal1.hacerSonido();
+        animal2.hacerSonido();
+    }
+}
 
