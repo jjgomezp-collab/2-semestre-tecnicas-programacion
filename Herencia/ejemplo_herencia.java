e77e6a6406284b320d5019b3c3f580519a800d74
Jostyn Javier Gomez Paredes <jj.gomezp@uea.edu.ec>
2025-12-06 15:23:54 -0600

    Update ejemplo_herencia.java

--- Herencia/ejemplo_herencia.java
+++ Herencia/ejemplo_herencia.java
@@ -1,1 +1,20 @@
+class Vehiculo {
+    public void arrancar() {
+        System.out.println("El vehículo está arrancando...");
+    }
+}
+
+class Carro extends Vehiculo {
+    public void tocarBocina() {
+        System.out.println("El carro hace: Beep beep!");
+    }
+}
+
+public class ejemplo_herencia {
+    public static void main(String[] args) {
+        Carro miCarro = new Carro();
+        miCarro.arrancar();
+        miCarro.tocarBocina();
+    }
+}
 
