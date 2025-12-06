f90779c93cb303d7ecdd370e14e1ff4f228d2e60
Jostyn Javier Gomez Paredes <jj.gomezp@uea.edu.ec>
2025-12-06 15:00:21 -0600

    Update ejemplo_abstraccion.java

--- Abstraccion/ejemplo_abstraccion.java
+++ Abstraccion/ejemplo_abstraccion.java
@@ -1,1 +1,23 @@
+abstract class Animal {
+    public abstract void hacerSonido();
+
+    public void dormir() {
+        System.out.println("El animal está durmiendo...");
+    }
+}
+
+class Perro extends Animal {
+    @Override
+    public void hacerSonido() {
+        System.out.println("El perro hace: Guau guau!");
+    }
+}
+
+public class ejemplo_abstraccion {
+    public static void main(String[] args) {
+        Animal miPerro = new Perro();
+        miPerro.hacerSonido();
+        miPerro.dormir();
+    }
+}
 
